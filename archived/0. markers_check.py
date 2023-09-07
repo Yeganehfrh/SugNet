154c4fe498ae11a37026e038b0049d151008fcf2
Yeganeh Farahzadi <36996819+Yeganehfrh@users.noreply.github.com>
2023-09-07 18:19:20 +0200

    cleanups

--- archived/0. markers_check.py
+++ archived/0. markers_check.py
@@ -1,19 +1,12 @@
-#%%
+# %%
 # This code groups EEG data based on the condition of their markers.
 # This is a necessary step before bidsifying.
 
 # Setup
-from mne.utils import docs
 import numpy as np
 import mne
 import os.path as op
 import pandas as pd
-import matplotlib.pyplot as plt
-from scipy import signal
-from scipy.fft import fft, fftshift
-from mne_bids import write_raw_bids, BIDSPath, print_dir_tree, inspect_dataset, mark_bad_channels
-from mne_bids.stats import count_events
-import pickle
 
 eeg_dir = '/Users/yeganeh/Documents/Raw Files/PLB_HYP_OTKA/Live Sessions'
 docs_dir = '/Users/yeganeh/Codes/otka-preprocessing/docs'
@@ -21,7 +14,7 @@ docs_dir = '/Users/yeganeh/Codes/otka-preprocessing/docs'
 # open ids map between eeg, behavioral and bids files
 fname = op.join(docs_dir, 'eeg_bh_bids_idmap.csv')
 total_ids_map = pd.read_csv(fname)
-total_ids_map.rename(columns={'Unnamed: 0':'eeg_ids'}, inplace=True)
+total_ids_map.rename(columns={'Unnamed: 0': 'eeg_ids'}, inplace=True)
 total_ids_map.set_index('eeg_ids', inplace=True, drop=True)
 
 # ---------------------- group1: data that are already analyzed in the first round
@@ -33,9 +26,10 @@ chunck1 = [i for i in chunck1 if i.isdigit()]
 imp_trg = [2152714, 2160311, 215711, 2152014, 2160314, 21052511, 2131811, 2131814] 
 [chunck1.remove(str(i)) for i in imp_trg]
 
-#%%
-# ---------------------- group2: finding data that are not analyzed in chucnk1 + data that their marker description is not as it should but the durations between markers are correct.
-## get the id of the participants who did not come to hypnotizability session
+# %%
+# ---------------------- group2: finding data that are not analyzed in chucnk1 + data that their marker description is
+# not as it should but the durations between markers are correct.
+# # get the id of the participants who did not come to hypnotizability session
 bh_data = pd.read_excel(op.join(docs_dir, 'data_with_hypnotizability.xlsx'), header=1)
 bh_data.set_index('index', inplace=True)
 ind_1 = bh_data[pd.isna(bh_data.hypnotizability_total)].index
@@ -48,7 +42,7 @@ group2.append('plb-hyp-live21031114')
 # impaired triggers in group2
 imp_trg2 = [2151211, 2151114, 21051811, 21052814, 21060111, 216811]
 
-#%%
+# %%
 # inspect the distance between triggers
 [imp_trg.append(i) for i in imp_trg2]
 
@@ -56,11 +50,9 @@ diffs = {}
 for key in imp_trg:
     diffs[key] = []
 
-markers_code = {'baseline_1':334.0,
-'intro_1':108.0, 'intro_2':103.0,
- 'induction_1':337.0, 'induction_2':336.0, 'induction_3':331.0, 'induction_4':363.0,
- 'experience':416.0,
- 'baseline_2':343.0}
+markers_code = {'baseline_1': 334.0, 'intro_1': 108.0, 'intro_2': 103.0, 'induction_1': 337.0,
+                'induction_2': 336.0, 'induction_3': 331.0, 'induction_4': 363.0, 'experience': 416.0,
+                'baseline_2': 343.0}
 
 # Open data, find the relation between triggers, and save them in adocument
 for sub_id in imp_trg:
@@ -73,10 +65,11 @@ diffs_df = pd.DataFrame(dict([(k, pd.Series(v)) for k, v in diffs.items()]))
 diffs_df = round(diffs_df)
 diffs_df
 
-#%%
+# %%
 # --------------------------- group3: the duration between the data is not ok
-# ok! the duration between data is most of the time ok: from my investigation in the last part data will be divided in two groups:
-# 1. duration of first baseline is ok
+# the duration between triggers is mostly fine:
+# from my investigation in the last part data will be divided in two groups:
+# 1. The duration of the first baseline is ok
 inact_baselines = []
 for col in diffs_df.columns:
     temp = diffs_df.loc[:, col].dropna().astype('int')
@@ -86,14 +79,12 @@ for col in diffs_df.columns:
 
 # Check if this group can be analyzed by referencing the last trigger (Stimulus 9):
 
-
 # 2. duration of first baseline is not ok: should be estimated by the behavioral data
 # [imp_trg.remove(i) for i in inact_baselines]
 imp_1base = [2152714, 2160311, 2160314, 21052511, 2151211, 21051811]
 
 # 3. 216811 that the duration of the first experience is not ok
-# and the 2152014 that has trigger for only the first 
-
+# and the 2152014 that has trigger for only the first
 
 # --------------------------- group4: software crash, bathroom break, dipoles was detected in the cap
 ['plb-hyp-live213914', 'plb-hyp-live2139142', 2152014]
